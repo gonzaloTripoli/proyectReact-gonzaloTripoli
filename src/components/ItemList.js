f4acfa415f3f046cf5551e9a93c89b333a93f2a0
gonzaloTripoli <gonzalotripoli1@hotmail.com>
2022-08-08 18:18:30 -0300

    Fix

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,58 +2,16 @@ import Item from "./Item"
 import { useState, useEffect } from "react"
 
 const ItemList = ({ data }) => {
-    const [dataChange, setData] = useState([{
-        title: "cargando",
-        id: "cargando",
-        price: "cargando",
-        imageUrl: "/images/loading.gif"
-    }, {
-        title: "cargando",
-        id: "cargando",
-        price: "cargando",
-        imageUrl: "/images/loading.gif"
-    }])
-    useEffect(() => {
-        setTimeout(() => {
-            console.log("SSS")
-            setData(data)
-        }, 2000)
-    }, [data])
 
     return (
         <>
             <div className="grid grid-cols-5 grid-rows-6 gap-16 place-items-center w-max " >
-                <div className="row-start-1 w-44">
-                <Item data={dataChange[0]} />
-                </div>
-                <div className="row-start-1  w-full">
-                <Item data={dataChange[1]} />
-                </div>
-                <div className="row-start-1  w-full">
-                <Item data={dataChange[0]} />
-                </div>
-                <div className="row-start-1  w-full">
-                <Item data={dataChange[1]} />
-                </div>
-                <div className="row-start-1 w-full ">
-                <Item data={dataChange[1]} />
-                </div>
-                <div className="row-start-2 w-44">
-                <Item data={dataChange[0]} />
-                </div>
-                <div className="row-start-2  w-full">
-                <Item data={dataChange[1]} />
-                </div>
-                <div className="row-start-2  w-full">
-                <Item data={dataChange[0]} />
-                </div>
-                <div className="row-start-2  w-full">
-                <Item data={dataChange[1]} />
-                </div>
-                <div className="row-start-2 w-full ">
-                <Item data={dataChange[1]} />
-                </div>
-                
+                {data.map(product => <div className="row-start-1 w-44">
+                    <Item key={product.id} data={product} />
+                </div>)
+                }
+
+
             </div>
         </>
 
---
+++ src/utils/products.js
@@ -0,0 +1,41 @@
+export const products = [{
+    title: "iphone 13",
+    id: "1",
+    price: 1500,
+    imageUrl: "/images/product-images/iphone13.webp",
+    Stock: 40,
+    category: "phones"
+
+}, {
+    title: "iphone 12",
+    id: "2",
+    price: 1300,
+    imageUrl: "/images/product-images/iphone12.webp",
+    Stock: 40,
+    category: "phones"
+
+}, {
+    title: "iphone 11",
+    id: "3",
+    price: 1500,
+    imageUrl: "/images/product-images/iphone13.webp",
+    Stock: 40,
+    category: "phones"
+
+}, {
+    title: "iphone x",
+    id: "4",
+    price: 1300,
+    imageUrl: "/images/product-images/iphone12.webp",
+    Stock: 40,
+    category: "phones"
+
+}, {
+    title: "iphone 7",
+    id: "5",
+    price: 1300,
+    imageUrl: "/images/product-images/iphone12.webp",
+    Stock: 40,
+    category: "phones"
+
+}]
